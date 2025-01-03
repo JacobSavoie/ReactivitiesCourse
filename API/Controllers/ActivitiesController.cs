f7af575019d6871c24df9a26e0746a213b4da3ba
JacobSavoie <savoie_jacob@bah.com>
2025-01-03 13:20:11 -0500

    cleanup to startup class unit4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -27,5 +27,20 @@ namespace API.Controllers
             await Mediator.Send(new Create.Command { Activity = activity });
             return Ok();
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command { Activity = activity });
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await Mediator.Send(new Delete.Command { Id = id });
+            return Ok();
+        }
     }
 }
--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,34 +1,26 @@
 using MediatR;
 using Persistence;
-using Domain;
-using AutoMapper;
-
 
 namespace Application.Activities
 {
-    public class Edit
+    public class Delete
     {
         public class Command : IRequest
         {
-            public Activity Activity {get; set; }
+            public Guid Id { get; set; }
         }
-
         public class Handler : IRequestHandler<Command>
         {
-            private readonly IMapper _mapper;
             private readonly DataContext _context;
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context)
             {
-                _mapper = mapper;
                 _context = context;
             }
             public async Task Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Activity.Id);
-
-                _mapper.Map(request.Activity, activity);
-
-                await _context.SaveChangesAsync(); 
+                var activity = await _context.Activities.FindAsync(request.Id);
+                _context.Remove(activity);
+                await _context.SaveChangesAsync();
             }
         }
     }
